4cc37cd1dea6aeb03b5c25155a008ccda084e7f8
Ilase <ilya280505@gmail.com>
2024-08-15 16:28:05 +0500

    example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
-#include "foo.cpp"
+
+// dom
+#include <ftxui/dom/elements.hpp>
+// component
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/captured_mouse.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+
+using namespace ftxui;
 
 int main(){
-    std::string name = "Robert";
-    hello(name);
-    auto monke = monkey(123, "Mikle");
-    auto mname = monke.get_name(); 
-    hello(mname);
+    //auto main_window = Container::Horizontal({});
+    // auto renderer = Renderer(main_window, [&]{
+        
+    // });
+    //auto screen = Screen::Create(Dimension::Full(),Dimension::Full());
+    auto screen = ScreenInteractive::Fullscreen();
+    auto render_space = Renderer( [&] {
+            return vbox({
+                text("hello niger") ,
+                separator(),
+                text(":3")
+            }) | border;
+        }
+    );
+    screen.Loop(render_space);
     return 0;
 }
